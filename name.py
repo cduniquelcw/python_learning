6547e40ef507e17c9434e0b76c7d7782e0e20a3d
So <So@So-deMacBook-Air.local>
2018-09-22 13:01:46 +0800

    name_py_test
    name py test

---
+++ name.py
@@ -0,0 +1,13 @@
+name = "so"
+name_up=name.upper()
+name_low=name.lower()
+name_tit=name.title()
+print("Hello "+name_tit+", would you like to learn some Python today?")
+famous_name="Albert Einstein"
+message="a person who never made a mistake never tried anything new."
+print(famous_name+" once said, "+'"'+ message +'"')
+test_name=" wang jiaying sb "
+print(test_name)
+print(test_name.lstrip())
+print(test_name.rstrip())
+print(test_name.strip())
