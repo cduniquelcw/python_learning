79d7ca214ac4ea61ac6962a1a8b6b02459e53aa4
So <zhuxuaaa@qq.com>
2018-09-27 23:28:46 +0800

    if sentence

--- alien_color.py
+++ alien_color.py
@@ -15,3 +15,33 @@ if 'banana' in favorite_fruits:
 	print('You really like banana!')
 if 'peach' in favorite_fruits:
 	print('You really like peach!')
+
+users_name=['zx','zj','wxj','ljj','admin']
+#users_name=[]
+if users_name:
+	for user_name in sorted(users_name):
+		if user_name == 'admin':
+			print('Hello admin, would you like to see a status report?')
+		else:
+			print('Hello '+user_name+', thank you for logging again.')
+else:
+	print('We need to find some users.')
+
+current_users=users_name
+new_users=['Zx','zj','wap','ty','hlp',]
+for new_user in new_users:
+	if new_user.lower() in current_users:
+		print(new_user+': You need input other name.')
+	else:
+		print(new_user+': This name was not used.')
+
+number_list=list(range(1,10))
+for number in number_list:
+	if number == 1:
+		print(str(number)+'st')
+	elif number == 2:
+		print(str(number)+'nd')
+	elif number == 3:
+		print(str(number)+'rd')
+	else:
+		print(str(number)+'th')
