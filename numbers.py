1e17f4ab442063a21e20a6ea3855b7487bb35206
So <zhuxuaaa@qq.com>
2018-09-30 18:52:14 +0800

    9-30

--- numbers.py
+++ numbers.py
@@ -3,19 +3,11 @@ even_numbers = []
 odd_numbers = []
 print(numbers)
 while numbers:
-	for num in numbers:
-		print(num)
-		if num % 2 == 0:
-			num = numbers.pop(0)#弹出后导致索引对应的元素改变，最终结果不对，想办法修改
-			even_numbers.append(num)
-			print(num)
-			print('even')
-		else:
-			num = numbers.pop(0)
-			odd_numbers.append(num)#奇数
-			print(num)
-			print('odd')
-
+	num = numbers.pop(0)
+	if num % 2 == 0:
+		even_numbers.append(num)
+	else:
+		odd_numbers.append(num)#奇数
 print(even_numbers)
 print(odd_numbers)
 print(numbers)
--- while.py
+++ while.py
@@ -4,7 +4,9 @@ print('Pastrami was sold out.')
 
 while 'pastrami' in sandwich_order:
 	sandwich_order.remove('pastrami')
-for sandwich in sandwich_order:
+while sandwich_order:
+	sandwich=sandwich_order.pop(0)
 	print('I made your '+ sandwich +' sandwich.')#对于for循环，其中遍历是按照索引从1到-1来的，因此在遍历的过程中删除列表中的元素会导致1到-1对应元素的改变，不可避免出错
 	finished_sandwiches.append(sandwich)
+print(sandwich_order)
 print(finished_sandwiches)
