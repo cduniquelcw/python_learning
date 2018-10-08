6eab79abcadf2e2f502d9894a460c66f62fabd5d
So <zhuxuaaa@qq.com>
2018-10-09 00:20:54 +0800

    10-8修改

--- read_file1.py
+++ read_file1.py
@@ -1,10 +1,14 @@
-with open('learning_python.txt')as file:
-	whole_file=file
-	print(whole_file)
+with open('learning_python.txt')as file_objiect:
+	whole_file=file_objiect.read()
+	print(whole_file)#直接打印整个文件
 
-	lines=file.readlines()
+#with open('learning_python.txt')as file_objiect:------#重新读取文件
 	all_lines=''
-	for line in lines:
+	for line in open('learning_python.txt'):
+	#for line in file_objiect:------#需要在前面重新读取文件，不然就没有输出
 		all_lines+=line
-		print(line.lstrip())
-print(all_lines+'...')
+		line_change=line.replace('Python','C++')
+		print(line.strip())#遍历每一行并打印
+		print(line_change.strip())
+
+print(all_lines+'...')#各行储存起来，with外打印
